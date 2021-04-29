569c39b622515d4c07de3e9c06aafdefc02a8209
Jay Khatri <jaykhatrimail@gmail.com>
2021-04-29 11:47:24 -0400

    changes

--- scripts/get-docker.sh
+++ scripts/get-docker.sh
@@ -2,8 +2,13 @@
 command_present() {
   type "$1" >/dev/null 2>&1
 }
-
+echo "[highlight] Installing docker"
 if ! command_present wget && command_present yum; then
   sudo yum install wget
 fi
 wget -qO- https://get.docker.com/ | sh
+echo "[highlight] Creating docker user group"
+sudo groupadd docker
+echo "[highlight] Adding the current user to the docker group"
+sudo usermod -aG docker ${USER}
+echo "[highlight] Logout of this ssh session and log back in to see these changes..."
--- scripts/set-docker-permissions.sh
+++
@@ -1,5 +0,0 @@
-echo "[highlight] Creating docker user group"
-sudo groupadd docker
-echo "[highlight] Adding the current user to the docker group"
-sudo usermod -aG docker ${USER}
-echo "[highlight] Logout of this ssh session and log back in to see these changes..."
