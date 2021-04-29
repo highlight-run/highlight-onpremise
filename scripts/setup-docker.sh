354a4aac21aa6b4f40ce670d829b081661d32c40
Jay Khatri <jaykhatrimail@gmail.com>
2021-04-29 12:22:55 -0400

    change

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -1,6 +1,14 @@
-
-echo "[highlight] Starting the docker daemon..."
-sudo systemctl start docker
-echo "[highlight] Building the Highlight docker image."
-docker build -t highlight-onprem .
-
+# adapted from https://askubuntu.com/questions/459402/how-to-know-if-the-running-platform-is-ubuntu-or-centos-with-help-of-a-bash-scri
+command_present() {
+  type "$1" >/dev/null 2>&1
+}
+echo "[highlight] Installing docker"
+if ! command_present wget && command_present yum; then
+  sudo yum install wget
+fi
+wget -qO- https://get.docker.com/ | sh
+echo "[highlight] Creating docker user group"
+sudo groupadd docker
+echo "[highlight] Adding the current user to the docker group"
+sudo usermod -aG docker ${USER}
+echo "[highlight] Logout of this ssh session and log back in to see these changes..."
