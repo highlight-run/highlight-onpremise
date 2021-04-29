fb69f6ca3da25c9591ea5012c9c7657222f85fa9
Jay Khatri <jaykhatrimail@gmail.com>
2021-04-29 11:43:51 -0400

    edit permissions to sh scripts

--- scripts/set-docker-permissions.sh
+++ scripts/set-docker-permissions.sh
@@ -2,3 +2,4 @@ echo "[highlight] Creating docker user group"
 sudo groupadd docker
 echo "[highlight] Adding the current user to the docker group"
 sudo usermod -aG docker ${USER}
+echo "[highlight] Logout of this ssh session and log back in to see these changes..."
--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -1,3 +1,6 @@
+
+echo "[highlight] Starting the docker daemon..."
 sudo systemctl start docker
+echo "[highlight] Building the Highlight docker image."
 docker build -t highlight-onprem .
 
