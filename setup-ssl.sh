6b09f818ba1d907ce29e0121a22e8efef37236fb
Jay Khatri <jaykhatrimail@gmail.com>
2021-05-14 16:45:26 -0400

    rename sample conf file

--- setup-ssl.sh
+++ setup-ssl.sh
@@ -3,7 +3,7 @@ mkdir $(pwd)/dhparam && sudo openssl dhparam -out $(pwd)/dhparam/dhparam-2048.pe
 echo "What is the domain for this Highlight deployment? (something like highlight.your-company.com)"
 read company_domain
 echo "[highlight] Creating nginx config file..."
-sed "s/COMPANY_DOMAIN/$company_domain/g" 'nginx-conf/sample-nginx.conf' > 'nginx-conf/nginx.conf'
+sed "s/COMPANY_DOMAIN/$company_domain/g" 'nginx-conf/nginx.conf.sample' > 'nginx-conf/nginx.conf'
 echo "What is your email for ssl validation? (something like xyz@your-company.com)"
 read company_email
 echo "[highlight] Templating docker compose file..."
