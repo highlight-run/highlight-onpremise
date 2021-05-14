ddb0f0578feb85b4aafc057dca79452038f13cf4
Jay Khatri <jaykhatrimail@gmail.com>
2021-05-14 16:26:15 -0400

    changes

--- setup-ssl.sh
+++ setup-ssl.sh
@@ -1,9 +1,13 @@
 echo "[highlight] Creating diffie=helman key for ssl..."
 mkdir $(pwd)/dhparam && sudo openssl dhparam -out $(pwd)/dhparam/dhparam-2048.pem 2048
-echo "What is your Highlight Company Domain? (something like xyz.your-company.com)"
+echo "What is the domain for this Highlight deployment? (something like highlight.your-company.com)"
 read company_domain
 echo "[highlight] Creating nginx config file..."
 sed "s/COMPANY_DOMAIN/$company_domain/g" 'nginx-conf/sample-nginx.conf' > 'nginx-conf/nginx.conf'
+echo "What is your email for ssl validation? (something like xyz@your-company.com)"
+read company_email
+echo "[highlight] Templating docker compose file..."
+sed -i "" -e "s/COMPANY_DOMAIN/$company_domain/g" -e "s/COMPANY_EMAIL/$company_email/g" 'docker-compose.yml'
 echo "[highlight] Done!"
 
 
